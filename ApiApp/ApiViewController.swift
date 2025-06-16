6185f575d090adf393f82ab575c4daae97f7a18f
yasusi.bando <bando057@outlook.jp>
2025-06-16 13:29:19 +0900

    練習中

---
+++ ApiApp/ApiViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ApiViewController.swift
+//  ApiApp
+//
+//  Created by 坂東康史 on 2025/06/16.
+//
+
+import UIKit
+
+class ApiViewController: UIViewController {
+
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
